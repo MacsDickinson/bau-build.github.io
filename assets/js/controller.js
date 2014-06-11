9668e087533b0dcbe7bed6d78936c11476c5216a
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-06-11 23:39:03 +0100

    initial commit

---
+++ assets/js/controller.js
@@ -0,0 +1,52 @@
+var Site = angular.module('Site', []);
+
+Site.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'mainController'
+    })
+
+    .when('/quick-start', {
+        templateUrl: 'pages/quick-start.html',
+        controller: 'quickStartController'
+    })
+
+    .when('/plugins', {
+        templateUrl: 'pages/plugins.html',
+        controller: 'pluginsController'
+    })
+
+    .when('/docs', {
+        templateUrl: 'pages/docs.html',
+        controller: 'docsController'
+    })
+
+    .when('/community', {
+        templateUrl: 'pages/community.html',
+        controller: 'communityController'
+    })
+    // if (window.history && window.history.pushState) {
+    //     $locationProvider.html5Mode(true);
+    // }
+});
+
+/*  Controllers
+ *
+ **********************************/
+
+Site.controller('homeController', function($scope) {});
+
+Site.controller('quickStartController', function($scope) {});
+
+Site.controller('pluginsController', function($scope) {});
+
+Site.controller('docsController', function($scope, $http) {
+    // Load docs
+    $http.get('models/docs.json').success(function(data) {
+        $scope.docs = data.docs;
+    });
+});
+
+Site.controller('communityController', function($scope) {});
---
+++ assets/js/site.js
@@ -0,0 +1,57 @@
+$(window).scroll(function() {
+    setMenuStuck();
+    setActiveContent();
+    $('.site-title').css('margin-top', ((parseInt($(window).scrollTop()) / 3) + 72) + 'px');
+});
+
+function setMenuStuck() {
+    if ($("#masthead").length > 0) {
+        if ($(window).scrollTop() > $("#masthead").outerHeight()) {
+            $('.menu.main').addClass('stuck');
+        } else {
+            $('.menu.main').removeClass('stuck');
+        };
+        if ($(window).scrollTop() > $("#masthead").outerHeight()) {
+            $('.side.menu').addClass('stuck');
+        } else {
+            $('.side.menu').removeClass('stuck');
+        };
+    } else {
+        $('.menu.main').addClass('stuck');
+        $('.side.menu').addClass('stuck');
+    }
+}
+
+function setActiveContent() {
+    $('.side.menu li span').each(function() {
+        var elementClicked = $(this).attr("href");
+        var target = $(elementClicked).offset().top;
+        var height = $(elementClicked).outerHeight();
+
+        if ($(window).scrollTop() >= target && $(window).scrollTop() < target + height) {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+    });
+    if ($('.side.menu li span.active').length == 0) {
+        $('.side.menu li span').first().addClass('active');
+    };
+}
+$(function() {
+    setActiveContent();
+    setMenuStuck();
+    $('.side.menu span').not('[target="_blank"]').click(function() {
+        e.preventDefault();
+        var elementClicked = $(this).attr("href");
+        var destination = $(elementClicked).offset().top;
+        $("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination
+        }, 500);
+
+        if (history != null)
+            history.replaceState(null, '', elementClicked);
+
+        return false;
+    });
+});
